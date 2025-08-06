aebb67058fb4dae45099495f5ca0576b26c67cd8
tpcwebbd-ops <tpc.web.bd@example.com>
2025-08-06 12:56:53 +0600

    done @ top nav

--- src/components/global/nav/Nav1/CustomNav1.tsx
+++ src/components/global/nav/Nav1/CustomNav1.tsx
@@ -6,23 +6,27 @@
 |-----------------------------------------
 */
 
-import CustomLink from '../CustomLink';
+import Link from 'next/link';
+import { FaFacebook } from 'react-icons/fa';
 
 const CustomNav1 = () => {
   type links = {
     name: string;
     url: string;
   }[];
-  const links: links = [
-    { name: 'Home', url: '/' },
-    { name: '+8801786 558855', url: '/' },
-  ];
+  const links: links = [{ name: '+8801786 558855', url: '/' }];
 
   return (
-    <nav className="w-full flex gap-4 items-center justify-end text-center px-4 bg-slate-700 text-slate-400">
-      {links.map((i, idx) => (
-        <CustomLink i={i} key={i.name + idx} />
-      ))}
+    <nav className="w-full flex gap-4 items-center justify-end text-center px-4 bg-slate-700 text-slate-50">
+      <div className="mx-auto container flex items-center justify-end gap-4">
+        <div>+880 1786 558855</div>
+        <Link href="https://www.facebook.com/groups/ielts.sharif.russel" target="_blank" className="flex gap-2 items-center justify-center hover:text-accent">
+          <FaFacebook /> Group
+        </Link>
+        <Link href="https://www.facebook.com/TestPrepCenter.BD/" target="_blank" className="flex gap-2 items-center justify-center hover:text-accent">
+          <FaFacebook /> Pgee
+        </Link>
+      </div>
     </nav>
   );
 };
