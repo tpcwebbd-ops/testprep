616cacf5738973794cc44dceea8a49558a559f53
tpcwebbd-ops <tpc.web.bd@example.com>
2025-08-06 13:55:34 +0600

    done @ add dashboard

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -6,7 +6,7 @@
 |-----------------------------------------
 */
 
-import AuthCheckingComponent from './dashboard-utils/AuthChecking';
+// import AuthCheckingComponent from './dashboard-utils/AuthChecking';
 // import SiteNavLayoutClickV1 from './dashboard-utils/site-nav/site-nav-v1/SiteNavLayoutClickV1';
 // import SiteNavLayoutClickV2 from './dashboard-utils/site-nav/site-nav-v2/SiteNavLayoutClickV2';
 import SiteNavLayoutClickV3 from './dashboard-utils/site-nav/site-nav-v3/SiteNavLayoutClickV3';
@@ -18,11 +18,25 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <AuthCheckingComponent redirectUrl="/dashboard">
+    <div>
       {/* <SiteNavLayoutClickV1> {children}</SiteNavLayoutClickV1> */}
       {/* <SiteNavLayoutClickV2> {children}</SiteNavLayoutClickV2> */}
       <SiteNavLayoutClickV3> {children}</SiteNavLayoutClickV3>
-      {/* <SiteNavLayoutClickV4> {children}</SiteNavLayoutClickV4> */}
-    </AuthCheckingComponent>
+      {/* <SiteNavLayoutClickV4> {children}</SiteNavLayoutClickV4>; */}
+    </div>
   );
 }
+// export default function RootLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode;
+// }>) {
+//   return (
+//     <AuthCheckingComponent redirectUrl="/dashboard">
+//       {/* <SiteNavLayoutClickV1> {children}</SiteNavLayoutClickV1> */}
+//       {/* <SiteNavLayoutClickV2> {children}</SiteNavLayoutClickV2> */}
+//       <SiteNavLayoutClickV3> {children}</SiteNavLayoutClickV3>
+//       {/* <SiteNavLayoutClickV4> {children}</SiteNavLayoutClickV4> */}
+//     </AuthCheckingComponent>
+//   );
+// }
