d9f92b6f9e5b4395fd1a645ddfcf931de82be0d8
tpcwebbd-ops <tpc.web.bd@example.com>
2025-08-20 14:39:57 +0600

    done @ fix variabale type in IELTS course

--- src/app/dashboard/course/ielts/store/StoreConstants.ts
+++ src/app/dashboard/course/ielts/store/StoreConstants.ts
@@ -3,9 +3,9 @@ import { IELTScourse } from '../api/v1/model';
 export const baseICoursesPerPage = 2;
 export const queryParams = { q: '', page: 1, limit: baseICoursesPerPage };
 export const pageLimitArr: number[] = [baseICoursesPerPage, 10, 50, 100, 200];
-export const select_5_000___: string = 'Pending';
-export const coursesSelectorArr = [select_5_000___, 'Private', 'Public'];
-export type ISelect_6_000___ = 'Pending' | 'Private' | 'Public';
+export const selectCourseState: string = 'Pending';
+export const coursesSelectorArr = [selectCourseState, 'Private', 'Public'];
+export type ISelectCourseStatus = 'Pending' | 'Private' | 'Public';
 export const defaultCoursesData: IELTScourse = {
   lectureTitle: '',
   lectureNo: 0,
