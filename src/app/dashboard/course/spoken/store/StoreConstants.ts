421285d3d05136884df351fe800fdd7c569dee5f
tpcwebbd-ops <tpc.web.bd@example.com>
2025-08-20 15:45:59 +0600

    done @ add note

--- src/app/dashboard/course/spoken/store/StoreConstants.ts
+++ src/app/dashboard/course/spoken/store/StoreConstants.ts
@@ -3,9 +3,9 @@ import { SpokenCourse } from '../api/v1/model';
 export const baseICoursesPerPage = 2;
 export const queryParams = { q: '', page: 1, limit: baseICoursesPerPage };
 export const pageLimitArr: number[] = [baseICoursesPerPage, 10, 50, 100, 200];
-export const select_5_000___: string = 'Pending';
-export const coursesSelectorArr = [select_5_000___, 'Private', 'Public'];
-export type ISelect_6_000___ = 'Pending' | 'Private' | 'Public';
+export const selectSpokenStatus: string = 'Pending';
+export const coursesSelectorArr = [selectSpokenStatus, 'Private', 'Public'];
+export type ISelectSpokenStatus = 'Pending' | 'Private' | 'Public';
 export const defaultCoursesData: SpokenCourse = {
   lectureTitle: '',
   lectureNo: 0,
