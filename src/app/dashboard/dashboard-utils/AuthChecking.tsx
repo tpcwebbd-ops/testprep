774a4bd6d05fc936eb54dc858746f189c7cf8b57
tpcwebbd-ops <tpc.web.bd@example.com>
2025-08-25 19:40:09 +0600

    update auth

--- src/app/dashboard/dashboard-utils/AuthChecking.tsx
+++ src/app/dashboard/dashboard-utils/AuthChecking.tsx
@@ -33,7 +33,7 @@ export default function AuthCheckingComponent({ redirectUrl = '/' as string, chi
     return <p>Loading session...</p>;
   }
   if (status === 'authenticated' && children) {
-    const allEmailsgiveAccess = ['tpc.web.bd@gmail.com', 'toufiquer.0@mgmail.com', 'reng32@gmail.com'];
+    const allEmailsgiveAccess = ['tpc.web.bd@gmail.com', 'toufiquer.0@mgmail.com', 'reng32@gmail.com', 'ctestprep@gmail.com'];
 
     const isAdmin = allEmailsgiveAccess.includes(data.email || '');
     if (!isAdmin) {
