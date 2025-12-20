7022cf609af15ab356aa8a7b9b887143233286c6
tpcwebbd-ops <tpc.web.bd@example.com>
2025-12-20 18:55:45 +0600

    done @ Update roles api with authorization

--- src/app/api/roles/v1/route.ts
+++ src/app/api/roles/v1/route.ts
@@ -1,75 +1,93 @@
 import { handleRateLimit } from '@/app/api/utils/rate-limit';
-import {
-    getRoles,
-    createRole,
-    updateRole,
-    deleteRole,
-    getRoleById,
-    bulkUpdateRoles,
-    bulkDeleteRoles,
-} from './controller';
+import { getRoles, createRole, updateRole, deleteRole, getRoleById, bulkUpdateRoles, bulkDeleteRoles } from './controller';
 
 import {
-    formatResponse,
-//    handleTokenVerify,
-    IResponse,
+  formatResponse,
+  //    handleTokenVerify,
+  IResponse,
 } from '@/app/api/utils/jwt-verify';
+import { isUserHasAccessByRole, IWantAccess } from '../../utils/is-user-has-access-by-role';
 
 // GET all Roles
 export async function GET(req: Request) {
-    const rateLimitResponse = handleRateLimit(req);
-    if (rateLimitResponse) return rateLimitResponse;
+  const rateLimitResponse = handleRateLimit(req);
+  if (rateLimitResponse) return rateLimitResponse;
 
-//    const tokenResponse = handleTokenVerify(req);
-//   if (tokenResponse) return tokenResponse;
+  //    const tokenResponse = handleTokenVerify(req);
+  //   if (tokenResponse) return tokenResponse;
 
-    const id = new URL(req.url).searchParams.get('id');
-    const result: IResponse = id
-        ? await getRoleById(req)
-        : await getRoles(req);
-    return formatResponse(result.data, result.message, result.status);
+  const wantToAccess: IWantAccess = {
+    db_name: 'Role',
+    access: 'read',
+  };
+  const isAccess = await isUserHasAccessByRole(wantToAccess);
+
+  if (isAccess) return isAccess;
+
+  const id = new URL(req.url).searchParams.get('id');
+  const result: IResponse = id ? await getRoleById(req) : await getRoles(req);
+  return formatResponse(result.data, result.message, result.status);
 }
 
 // CREATE Role
 export async function POST(req: Request) {
-    const rateLimitResponse = handleRateLimit(req);
-    if (rateLimitResponse) return rateLimitResponse;
+  const rateLimitResponse = handleRateLimit(req);
+  if (rateLimitResponse) return rateLimitResponse;
 
-//    const tokenResponse = handleTokenVerify(req);
-//    if (tokenResponse) return tokenResponse;
+  //    const tokenResponse = handleTokenVerify(req);
+  //    if (tokenResponse) return tokenResponse;
 
-    const result = await createRole(req);
-    return formatResponse(result.data, result.message, result.status);
+  const wantToAccess: IWantAccess = {
+    db_name: 'Role',
+    access: 'create',
+  };
+  const isAccess = await isUserHasAccessByRole(wantToAccess);
+
+  if (isAccess) return isAccess;
+
+  const result = await createRole(req);
+  return formatResponse(result.data, result.message, result.status);
 }
 
 // UPDATE Role
 export async function PUT(req: Request) {
-    const rateLimitResponse = handleRateLimit(req);
-    if (rateLimitResponse) return rateLimitResponse;
+  const rateLimitResponse = handleRateLimit(req);
+  if (rateLimitResponse) return rateLimitResponse;
 
-//    const tokenResponse = handleTokenVerify(req);
-//    if (tokenResponse) return tokenResponse;
+  //    const tokenResponse = handleTokenVerify(req);
+  //    if (tokenResponse) return tokenResponse;
+  const wantToAccess: IWantAccess = {
+    db_name: 'Role',
+    access: 'update',
+  };
+  const isAccess = await isUserHasAccessByRole(wantToAccess);
 
-    const isBulk = new URL(req.url).searchParams.get('bulk') === 'true';
-    const result = isBulk
-        ? await bulkUpdateRoles(req)
-        : await updateRole(req);
+  if (isAccess) return isAccess;
 
-    return formatResponse(result.data, result.message, result.status);
+  const isBulk = new URL(req.url).searchParams.get('bulk') === 'true';
+  const result = isBulk ? await bulkUpdateRoles(req) : await updateRole(req);
+
+  return formatResponse(result.data, result.message, result.status);
 }
 
 // DELETE Role
 export async function DELETE(req: Request) {
-    const rateLimitResponse = handleRateLimit(req);
-    if (rateLimitResponse) return rateLimitResponse;
+  const rateLimitResponse = handleRateLimit(req);
+  if (rateLimitResponse) return rateLimitResponse;
 
-//    const tokenResponse = handleTokenVerify(req);
-//    if (tokenResponse) return tokenResponse;
+  //    const tokenResponse = handleTokenVerify(req);
+  //    if (tokenResponse) return tokenResponse;
 
-    const isBulk = new URL(req.url).searchParams.get('bulk') === 'true';
-    const result = isBulk
-        ? await bulkDeleteRoles(req)
-        : await deleteRole(req);
+  const wantToAccess: IWantAccess = {
+    db_name: 'Role',
+    access: 'delete',
+  };
+  const isAccess = await isUserHasAccessByRole(wantToAccess);
 
-    return formatResponse(result.data, result.message, result.status);
+  if (isAccess) return isAccess;
+
+  const isBulk = new URL(req.url).searchParams.get('bulk') === 'true';
+  const result = isBulk ? await bulkDeleteRoles(req) : await deleteRole(req);
+
+  return formatResponse(result.data, result.message, result.status);
 }
